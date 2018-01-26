b0161cdd5c6e4b30fe6e2d949d17b6dbdf128472
Lukas Nord <luke.nord@gmail.com>
2018-01-26 12:37:40 -0600

    Fixed bug with track-employee view not pulling immediately

--- server/public/scripts/controllers/track-employee.controller.js
+++ server/public/scripts/controllers/track-employee.controller.js
@@ -1,7 +1,7 @@
 myApp.controller('TrackEmployeeController', ['HermesService', function(HermesService) {
     const self = this;
     
-    
+    HermesService.getEmployees();
     // Data bind to HermesService:  
     self.employees = HermesService.employees;
     
