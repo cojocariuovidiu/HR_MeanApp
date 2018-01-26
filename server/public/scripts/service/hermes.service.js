bd18d8667fb67dd11bf5c4f93374f08edd2628d3
Lukas Nord <luke.nord@gmail.com>
2018-01-26 11:56:09 -0600

    created Hermes Get service method for getting all employees

--- server/public/scripts/service/hermes.service.js
+++ server/public/scripts/service/hermes.service.js
@@ -1,12 +1,25 @@
 myApp.service('HermesService',['$http', function($http) {
 console.log('inside service');
     const self = this;
-    self.employeeList = [];
-
+    self.employees = { list: [] };
 
 /* GET REQUESTS */
 
+//Get All Employees Service Method
+self.getEmployees = function(){
 
+    $http.get('/employees')
+        .then( function (response) {
+            console.log('Get response: ', response.data);
+            self.employees.list = response.data;
+            console.log('self.employees.list: ', self.games.list);
+            
+        })
+        .catch (function(response) {
+            console.log('error on Get: ', response); 
+        })
+
+}// end getEmployees
 
 
 
