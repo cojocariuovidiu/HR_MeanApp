ddb9a155d22178918d6383bbe7c9e264725b54fe
Darian N <darian.nasrabadi@gmail.com>
2018-01-26 11:09:12 -0600

    adding base functions and employee-list layout

--- server/public/scripts/controllers/track-employee.controller.js
+++ server/public/scripts/controllers/track-employee.controller.js
@@ -1,6 +1,13 @@
 myApp.controller('whateverControllerthisis', ['HermesService', function(HermesService) {
     const self = this;
 
+    
+    self.editStatus = HermesService.editStatus;
+    
+    self.showDetails = function(){
+        self.editStatus = !self.editStatus; //when clicked it can either open the details or close it if they choose.
+    
+    }
 
 
 }]);
