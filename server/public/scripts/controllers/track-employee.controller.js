635595c24600a1ec8fdb4aa9b3cb285ce313ea5f
Lukas Nord <luke.nord@gmail.com>
2018-01-26 11:35:34 -0600

    ng-repeat and table built out

--- server/public/scripts/controllers/track-employee.controller.js
+++ server/public/scripts/controllers/track-employee.controller.js
@@ -1,6 +1,55 @@
 myApp.controller('TrackEmployeeController', ['HermesService', function(HermesService) {
     const self = this;
 
+    self.employees = [
+
+            {
+                firstName: 'Lukas',
+                lastName: 'Nord',
+                employeeId: 1234,
+                title: 'Programmer',
+                salary: 10000,
+                
+            },
+            {
+                firstName: 'Patrick',
+                lastName: 'Connelly',
+                employeeId: 4321,
+                title: 'Software Developer',
+                salary: 11000,
+            },
+            {
+                firstName: 'Darian',
+                lastName: 'Nasrabadi',
+                employeeId: 500,
+                title: 'Software Engineer',
+                salary: 12000,
+            },
+            {
+                firstName: 'Alaethia',
+                lastName: 'Dvoracek',
+                employeeId: 800,
+                title: 'Engineering Programmer',
+                salary: 13000,
+            }
+
+
+
+    ];
+
+
+
+
+
+//example employee data
+    // {
+    //     firstName: {type: String, required: true},
+    //     lastName: {type: String, required: true},
+    //     employeeId: {type: Number, unique: true, required: true},
+    //     title: {type: String, required: true},
+    //     salary: {type: Number, required: true},
+    //     hireDate: {type: Date, default: Date.now}
+    // }
 
 
 
@@ -11,9 +60,6 @@ myApp.controller('TrackEmployeeController', ['HermesService', function(HermesSer
 
 
 
-
-
-    
 
 
 
