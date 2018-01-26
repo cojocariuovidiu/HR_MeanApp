e98ad252342686cb33ffc4ce1a796c6b303df858
Patrick Connelly <patrickconnelly95@gmail.com>
2018-01-26 11:59:27 -0600

    sample avg aggregate on controller;

--- server/public/scripts/controllers/reports.controller.js
+++ server/public/scripts/controllers/reports.controller.js
@@ -1,8 +1,33 @@
 myApp.controller('ReportController', ['HermesService', function(HermesService) {
     const self = this;
-    self
-    
-    self.employees = HermesService.employees;
+    self.titles = ['Clown', 'Monkey', 'Peach'];
 
+
+    for (let i = 0; i < self.employeeList.length; i++) {
+        self.titles.push(self.employeeList[i].title);
+    }
+
+    self.employeeList = HermesService.employeeList;
+
+    getAverage()
 
 }]);
+
+
+let pipeline = 
+[
+    {
+        "$group": {
+            "_id": "$title",
+            "avgSalary": {"$avg": "$salary"}
+        }
+    }
+]
+
+Employee.aggregate(pipeline, function(err, result){
+    if(err) {
+        res.send(String(err));
+    }
+    console.log('avg result ', result);
+    // res.send(result);
+})
